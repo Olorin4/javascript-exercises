9a3fd7acfbf20dede09671384850a0c79f0c9e41
Nick Kalas <nick.kalas@proton.me>
2024-03-27 22:33:48 +0200

    Add preliminary solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...num) {
+    for (let i = num.length - 1; i >= 0; i--) {
+        let index = array.indexOf(num[i]) ;
+        if (index !== -1) {
+            array.splice(index, 1);
+        }
+        while ((index = array.indexOf(num[i])) !== -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
+removeFromArray([1, 2, 2, 3], 2); // should remove 3 and return [1,3]
 
 // Do not edit below this line
 module.exports = removeFromArray;
