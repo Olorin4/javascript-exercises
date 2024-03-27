51ef77a3e6a6f20d77d1786ff81c4ac30d48077e
Nick Kalas <nick.kalas@proton.me>
2024-03-27 15:49:52 +0200

    Add preliminary repeat function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let num;
+    let string;
+    return repeatString = string.repeat(num);
 };
+
+repeatString('hey', 3)
+console.log(repeatString);
 
 // Do not edit below this line
 module.exports = repeatString;
