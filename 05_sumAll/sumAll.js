48593055e4a2f22989818603653f6ec923eafcf7
Nick Kalas <nick.kalas@proton.me>
2024-03-28 00:21:51 +0200

    Refactor solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -11,9 +11,6 @@ const sumAll = function (int1, int2) {
         return sum;
     }
 }
-    
-
-sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
