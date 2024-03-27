1d270e00c4305398295f434de81090136e1caea7
Nick Kalas <nick.kalas@proton.me>
2024-03-27 22:37:14 +0200

    Refactor solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,9 @@
 const removeFromArray = function (array, ...num) {
-    for (let i = num.length - 1; i >= 0; i--) {
-        let index = array.indexOf(num[i]) ;
-        if (index !== -1) {
-            array.splice(index, 1);
+    num.forEach(item => {
+        while (array.includes(item)) {
+            array.splice(array.indexOf(item), 1);
         }
-        while ((index = array.indexOf(num[i])) !== -1) {
-            array.splice(index, 1);
-        }
-    }
+    });
     return array;
 };
 removeFromArray([1, 2, 2, 3], 2); // should remove 3 and return [1,3]
