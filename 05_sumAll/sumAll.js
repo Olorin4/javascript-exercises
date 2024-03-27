a7459eaa2bd2c1452f3817a8d19cca28e726b9cf
Nick Kalas <nick.kalas@proton.me>
2024-03-28 00:21:34 +0200

    Refactor solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,17 +2,11 @@ const sumAll = function (int1, int2) {
     if (int1 < 0 || int2 <  0 || (typeof int1 !== 'number') || (typeof int2 !== 'number')) {
         return 'ERROR';
     } else {
+        const smaller = Math.min(int1, int2);
+        const larger = Math.max(int1, int2);
         let sum = 0;
-        if (int1 < int2) {
-            for (let i =int1; i <= int2; i++) {
-                sum +=  i ;
-            } 
-        } else if (int1 > int2) {
-            for (let i = int2; i <= int1; i++) {
-                sum += i;
-            }
-        } else {
-            sum = int1 + int2;
+        for (let i = smaller; i <= larger; i++) {
+            sum += i;
         }
         return sum;
     }
