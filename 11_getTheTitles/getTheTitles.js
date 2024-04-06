41787a80ae7596c5da5c01f9a77bcc9a5f82dba7
Nick Kalas <nick.kalas@proton.me>
2024-04-06 08:45:36 +0300

    Add solution to exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
+const getTheTitles = function (arr) {
+    return arr.map((obj) => obj["title"]);
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
