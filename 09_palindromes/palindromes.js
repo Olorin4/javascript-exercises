afc8b5fce57a6ed9131a56c76b72873685c9f636
Nick Kalas <nick.kalas@proton.me>
2024-04-06 07:44:20 +0300

    Add solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let alphanumericStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reversedStr = alphanumericStr.split('').reverse().join('');
+        return alphanumericStr === reversedStr;
 };
 
 // Do not edit below this line
