cd142994c5c82e3c72d993100804fcbecb13d769
Nick Kalas <nick.kalas@proton.me>
2024-04-06 09:43:14 +0300

    Add partial solution to exercise 12. To complete later

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
 
+const findTheOldest = function (people) {
+    let age = people.map(obj => (obj.yearOfDeath - obj.yearOfBirth));
+    let maxAge = Math.max(...age); // Find the maximum age
+    let indexOfMaxAge = age.indexOf(maxAge); // Find the index of the maximum age
+    return people[indexOfMaxAge];
 };
 
 // Do not edit below this line
