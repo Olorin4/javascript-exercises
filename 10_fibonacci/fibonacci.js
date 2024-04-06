bb190e1c77382260d21613c8e4acf405df793ecd
Nick Kalas <nick.kalas@proton.me>
2024-04-06 08:08:00 +0300

    Add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (N) {
+    let n = +N;
+    if (n <= 1 && n >= 0) {
+        return n;
+    } else if (n > 1) {
+        let fib = [0, 1];
+        for (let i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
